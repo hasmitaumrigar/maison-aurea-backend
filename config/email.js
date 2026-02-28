c876ed5a7c73ee19fb240982f328d3fb76f5ce42
Hasmita <hasmitadolly007@gmail.com>
2026-02-28 21:22:17 +0000

    fix smtp port 465

--- config/email.js
+++ config/email.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: '74.125.133.108',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
